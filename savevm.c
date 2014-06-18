4a7f0a56d93bee88e2a735fba4e6d0149a20eee6
Dr. David Alan Gilbert <dgilbert@redhat.com>
2014-06-18 17:39:11 +0100

    postcopy: OS support test
    Provide a check to see if the OS we're running on has all the bits
    needed for postcopy.
    
    Creates postcopy-ram.c which will get most of the other helpers we need.
    
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

--- savevm.c
+++ savevm.c
@@ -33,6 +33,7 @@
 #include "qemu/timer.h"
 #include "audio/audio.h"
 #include "migration/migration.h"
+#include "migration/postcopy-ram.h"
 #include "qemu/sockets.h"
 #include "qemu/queue.h"
 #include "sysemu/cpus.h"
@@ -1110,6 +1111,10 @@ static int loadvm_postcopy_handle_advise(MigrationIncomingState *mis,
     trace_loadvm_postcopy_handle_advise();
     if (ps != POSTCOPY_INCOMING_NONE) {
         error_report("CMD_POSTCOPY_ADVISE in wrong postcopy state (%d)", ps);
+        return -1;
+    }
+
+    if (!postcopy_ram_supported_by_host()) {
         return -1;
     }
 
