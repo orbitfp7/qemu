14ed81a971546a91fb56b16b14e45a95a37e3fe1
Dr. David Alan Gilbert <dgilbert@redhat.com>
2014-06-20 13:13:54 +0100

    postcopy: Incoming initialisation
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Reviewed-by: David Gibson <david@gibson.dropbear.id.au>
    Reviewed-by: Amit Shah <amit.shah@redhat.com>

--- migration/savevm.c
+++ migration/savevm.c
@@ -1279,6 +1279,12 @@ static int loadvm_postcopy_handle_advise(MigrationIncomingState *mis,
         return -1;
     }
 
+    if (ram_postcopy_incoming_init(mis)) {
+        return -1;
+    }
+
+    postcopy_state_set(POSTCOPY_INCOMING_ADVISE);
+
     return 0;
 }
 
