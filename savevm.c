886a391d4183d7678f3ba4dbbefc9df270349485
Dr. David Alan Gilbert <dgilbert@redhat.com>
2014-10-02 15:29:02 +0100

    Postcopy: Maintain sentmap and calculate discard
    Where postcopy is preceeded by a period of precopy, the destination will
    have received pages that may have been dirtied on the source after the
    page was sent.  The destination must throw these pages away before
    starting it's CPUs.
    
    Maintain a 'sentmap' of pages that have already been sent.
    Calculate list of sent & dirty pages
    Provide helpers on the destination side to discard these.
    
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

--- savevm.c
+++ savevm.c
@@ -1242,7 +1242,6 @@ static int loadvm_postcopy_ram_handle_discard(MigrationIncomingState *mis,
     }
     trace_loadvm_postcopy_ram_handle_discard_header(ramid, len);
     while (len) {
-        /* TODO - ram_discard_range gets added in a later patch
         uint64_t start_addr, end_addr;
         start_addr = qemu_get_be64(mis->file);
         end_addr = qemu_get_be64(mis->file);
@@ -1252,7 +1251,6 @@ static int loadvm_postcopy_ram_handle_discard(MigrationIncomingState *mis,
         if (ret) {
             return ret;
         }
-        */
     }
     trace_loadvm_postcopy_ram_handle_discard_end();
 
