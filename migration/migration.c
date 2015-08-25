90b788ca2940ef4044ee1eb7cfbbe5a9da0d9cc9
Dr. David Alan Gilbert <dgilbert@redhat.com>
2015-08-25 19:59:50 +0100

    Rename mis->file to from_src_file
    'file' becomes confusing when you have flows in each direction;
    rename to make it clear.
    This leaves just the main forward direction ms->file, which is used
    in a lot of places and is probably not worth renaming given the churn.
    
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

--- migration/migration.c
+++ migration/migration.c
@@ -87,7 +87,7 @@ MigrationIncomingState *migration_incoming_get_current(void)
 MigrationIncomingState *migration_incoming_state_new(QEMUFile* f)
 {
     mis_current = g_malloc0(sizeof(MigrationIncomingState));
-    mis_current->file = f;
+    mis_current->from_src_file = f;
     QLIST_INIT(&mis_current->loadvm_handlers);
 
     return mis_current;
